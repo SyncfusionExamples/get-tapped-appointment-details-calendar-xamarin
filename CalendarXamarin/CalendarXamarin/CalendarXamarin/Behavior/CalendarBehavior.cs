59ad54a462abad33b391a7131ec1a8c5ce19bbe6
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2021-01-13 17:08:32 +0530

    XAMARIN-35167 - Sample and screenshot added.

---
+++ CalendarXamarin/CalendarXamarin/CalendarXamarin/Behavior/CalendarBehavior.cs
@@ -0,0 +1,36 @@
+﻿using Syncfusion.SfCalendar.XForms;
+using Xamarin.Forms;
+
+namespace CalendarXamarin.Behavior
+{
+    class CalendarBehavior : Behavior<ContentPage>
+    {
+        SfCalendar calendar;
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+            calendar = bindable.FindByName<SfCalendar>("calendar");
+            calendar.OnCalendarTapped += Calendar_OnCalendarTapped;
+        }
+
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            calendar.OnCalendarTapped -= Calendar_OnCalendarTapped;
+        }
+
+        private void Calendar_OnCalendarTapped(object sender, CalendarTappedEventArgs e)
+        {
+            var appointmentCollection = e.SelectedAppointment as CalendarEventCollection;
+            if (appointmentCollection.Count > 0)
+            {
+                var appointment = appointmentCollection[0]; ;
+                App.Current.MainPage.DisplayAlert(appointment.Subject, appointment.StartTime.ToString("dd/MM/yyyy hh:mm tt"), "OK");
+            }
+            else
+            {
+                App.Current.MainPage.DisplayAlert("", "No Events", "OK");
+            }
+        }
+    }
+}
